63d8060d6c124b158a59af37b95eab1c500460e1
Josh. M <josh.macdonald@torontomu.ca>
2024-12-07 12:48:19 -0500

    Cleaned up

--- project_3_Step_2.py
+++ project_3_Step_2.py
@@ -19,20 +19,3 @@ model.export()
 results1 = model.predict("ardmega.jpg", save=True, imgsz=928)
 results2 = model.predict("arduno.jpg", save=True, imgsz=928)
 results3 = model.predict("rasppi.jpg", save=True, imgsz=928)
-
-
-
-# metrics = model.val()
-
-# model("ardmega.jpg")
-# model("arduno.jpg")
-# model("rasppi.jpg")
-
-# img1=results1[0].plot(font_size=40,pil=True)
-# cv.imwrite(('ardmega_out.jpg'), img1)
-# img2=results2[0].plot(font_size=15,pil=True)
-# cv.imwrite(('arduno_out.jpg'), img2)
-# img3=results3[0].plot(font_size=30,pil=True)
-# cv.imwrite(('rasppi_out.jpg'), img3)
-# cv.waitKey()
-# cv.destroyAllWindows()
